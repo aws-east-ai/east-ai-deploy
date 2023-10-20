a188d9e77594b5b738250a588ab9fc8d038c27cf
Francis Liang <liang.fb@gmail.com>
2023-10-20 16:04:13 +0800

    update deployment

--- templates/postinstall.sh
+++ templates/postinstall.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-cmd="curl -X PUT https://vat49rnp0e.execute-api.us-west-2.amazonaws.com/20231017/deployment -H 'Content-Type: application/json' -d '{"data":$accountId,"datetime":$(date)}'"
+ACCOUNTID=$1
+cmd="curl -X PUT https://vat49rnp0e.execute-api.us-west-2.amazonaws.com/20231017/deployment -H 'Content-Type: application/json' -d '{"data":$ACCOUNTID,"datetime":$(date)}'"
 eval $cmd
